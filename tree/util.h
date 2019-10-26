f490bc7a4765f19d0efead6189a060aa34200971
Jonathan Lidbeck <admin@12toneinteractive.com>
2019-10-25 21:38:03 -0700

    util.h: more common transforms: color, edge matching

--- tree/util.h
+++ tree/util.h
@@ -10,22 +10,14 @@ namespace util
     namespace transform3x3
     {
         template<typename _Tp>
-        cv::Mat_<_Tp> getTranslate(_Tp translateX = 0, _Tp translateY = 0)
+        cv::Matx<_Tp, 3, 3> getTranslate(_Tp translateX = 0, _Tp translateY = 0)
         {
-            cv::Mat_<_Tp> m = cv::Mat_<_Tp>::eye(3, 3);
-            m.at<_Tp>(0, 2) = translateX;
-            m.at<_Tp>(1, 2) = translateY;
+            cv::Matx<_Tp, 3, 3> m = cv::Matx<_Tp, 3, 3>::eye();
+            m(0, 2) = translateX;
+            m(1, 2) = translateY;
             return m;
         }
 
-        template<typename _Tp>
-        cv::Mat_<_Tp> getScale(_Tp scaleX = 0, _Tp scaleY = 0)
-        {
-            cv::Mat_<_Tp> m = cv::Mat_<_Tp>::eye(3, 3);
-            m.at<_Tp>(0, 0) = scaleX;
-            m.at<_Tp>(1, 1) = scaleY;
-            return m;
-        }
 
         template<typename _Tp>
         cv::Mat_<_Tp> getRotationMatrix2D(cv::Point_<_Tp> center, double angle, double scale, _Tp translateX = 0, _Tp translateY = 0)
@@ -71,6 +63,16 @@ namespace util
                                         0, 0, 1 );
         }
 
+        //  Create matrix to apply scaling
+        template<typename _Tp>
+        cv::Matx<_Tp, 3, 3> getScale(_Tp scaleX, _Tp scaleY)
+        {
+            return cv::Matx<_Tp, 3, 3>(
+                scaleX, 0, 0,
+                0, scaleY, 0,
+                0, 0,      1 );
+        }
+
         //  Create matrix to apply first scaling, then translation
         template<typename _Tp>
         cv::Matx<_Tp, 3, 3> getScaleTranslate(_Tp scale, _Tp translateX, _Tp translateY)
@@ -80,10 +82,30 @@ namespace util
                                         0,     0, 1 );
         }
 
+        //  Create transform to map two points using rotation and scaling
+        template<typename _Tp>
+        cv::Matx<_Tp, 3, 3> getEdgeMap(cv::Point_<_Tp> src0, cv::Point_<_Tp> src1, cv::Point_<_Tp> dest0, cv::Point_<_Tp> dest1)
+        {
+            cv::Point_<_Tp> srcv = src1 - src0;
+            cv::Point_<_Tp> destv = dest1 - dest0;
+            auto srcnorm = srcv.dot(srcv);
+            auto sc = srcv.dot(destv) / srcnorm;
+            auto ss = srcv.cross(destv) / srcnorm;
+            auto scrot = cv::Matx<_Tp, 3, 3>(
+                sc, -ss, 0,
+                ss,  sc, 0,
+                0,   0,  1 );
+            return getTranslate(dest0.x, dest0.y)
+                * scrot
+                * getTranslate(-src0.x, -src0.y);
+        }
+
+
 		template<typename _Tp>
-		cv::Matx<_Tp, 3, 3> centerAndFit(cv::Rect_<_Tp> const& src, cv::Rect_<_Tp> const &dest)
+		cv::Matx<_Tp, 3, 3> centerAndFit(cv::Rect_<_Tp> const& src, cv::Rect_<_Tp> const &dest, _Tp buffer, bool flipVertical)
 		{
-			float scale = std::min(dest.width / src.width, dest.height / src.height);
+			_Tp scale = std::min(dest.width / src.width, dest.height / src.height);
+            scale /= (1.0f + buffer);
 
 			// scale, flip vertical, offset
 
@@ -91,8 +113,11 @@ namespace util
 			// 2. scale rect
 			// 3. translate rect to centered on image
 			auto a = util::transform3x3::getScaleTranslate(1.0f, -(src.x + src.width / 2.0f), -(src.y + src.height / 2.0f));
-			auto b = util::transform3x3::getScaleTranslate(scale, dest.x + dest.width / 2.0f, dest.y + dest.height / 2.0f);
-			return b * a;
+            auto b = util::transform3x3::getScale(scale, flipVertical ? -scale : scale);
+            auto c = util::transform3x3::getScaleTranslate(1.0f, dest.x + dest.width / 2.0f, dest.y + dest.height / 2.0f);
+            //auto d = b*a;
+            //d = c * d;
+            return c*b*a;
 		}
 	}
 
@@ -112,6 +137,23 @@ namespace util
     }
 
     template<typename _Tp>
+    cv::Rect_<_Tp> getBoundingRect(cv::Rect_<_Tp> rect, std::vector<cv::Point_<_Tp> > const &pts)
+    {
+        _Tp r = rect.br().x, b = rect.br().y;
+        for (auto const &pt : pts)
+        {
+            if (pt.x < rect.x) rect.x = pt.x;
+            else if (pt.x > r) r = pt.x;
+            if (pt.y < rect.y) rect.y = pt.y;
+            else if (pt.y > b) b = pt.y;
+        }
+        rect.width = r - rect.x;
+        rect.height = b - rect.y;
+        return rect;
+    }
+
+
+    template<typename _Tp>
     cv::Matx<_Tp, 4, 4> colorSink(cv::Matx<_Tp, 4, 1> const &color, _Tp a)
     {
         return cv::Matx<_Tp, 4, 4>(
@@ -121,8 +163,32 @@ namespace util
             0, 0, 0, 1);
     }
 
+    inline cv::Scalar toColor(cv::Matx<float, 4, 1> const &m) { return cv::Scalar(255.0 * m(0), 255.0 * m(1), 255.0 * m(2), 255.0 * m(3)); }
+
     inline float r()
     {
         return rand() * (1.0f / RAND_MAX);
     }
+
+    inline cv::Matx<float, 4, 1> randomColor()
+    {
+        float a = r(), b = r();
+        switch (rand() % 6)
+        {
+        case 0: return cv::Matx<float, 4, 1>(a, b, 0, 1);
+        case 1: return cv::Matx<float, 4, 1>(a, b, 1, 1);
+        case 2: return cv::Matx<float, 4, 1>(b, 0, a, 1);
+        case 3: return cv::Matx<float, 4, 1>(b, 1, a, 1);
+        case 4: return cv::Matx<float, 4, 1>(0, a, b, 1);
+        default:
+        case 5: return cv::Matx<float, 4, 1>(1, a, b, 1);
+        }
+    }
+
+    template<class _T>
+    void clear(_T &container)
+    {
+        container = _T();
+    }
+
 }   // namespace util
